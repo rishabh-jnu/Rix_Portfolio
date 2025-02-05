efcc3e9b45096d819188b9ddf2a1b79dd61ebcd3
Your Name <rishabhyadav70070@gmail.com>
2025-02-05 16:38:39 +0530

    Portfolio Component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,27 +5,27 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "Health Management System",
-    img: "https://cdna.artstation.com/p/assets/images/images/020/887/488/large/inhyuk-lee-flash-forward-2.jpg?1569543312",
-    desc: " Mern Application for managing health records of patients and doctors.",
+    title: "Health Management",
+    img: "marvel1.jpg",
+    desc: " MERN Application for managing health records of patients and doctors.",
   },
   {
     id: 2,
-    title: "Next.js Blog",
-    img: "https://i.pinimg.com/736x/c2/ab/be/c2abbe14ddc7c483da1439fcf03bf88f.jpg",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "BlogApp",
+    img: "marvel2.jpg",
+    desc: "BlogApp is a full stack application where users can create, read, update and delete blogs.",
   },
   {
     id: 3,
-    title: "Vanilla JS App",
-    img: "https://i.pinimg.com/originals/2c/bf/e3/2cbfe360e9ca6f696e43dce47490f050.jpg",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "GymViteApp",
+    img: "marvel3.jpg",
+    desc: "GymViteApp is a MERN application where Gymrates can take refrence for gym workouts.",
   },
   {
     id: 4,
-    title: "Music App",
-    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/80a18798-fbc7-46e4-b0a6-24516482651f/dgtf373-afe65491-1e0c-4d91-956f-eaf3519645b6.jpg/v1/fill/w_894,h_894,q_70,strp/thor__god_of_thunder___3_by_alienfodder_dgtf373-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcLzgwYTE4Nzk4LWZiYzctNDZlNC1iMGE2LTI0NTE2NDgyNjUxZlwvZGd0ZjM3My1hZmU2NTQ5MS0xZTBjLTRkOTEtOTU2Zi1lYWYzNTE5NjQ1YjYuanBnIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.nEkel3Yq2IisZBkZwxuVCOmomTP69lG3uT1MVRp9Zfs",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "CarbonFootprint",
+    img: "thor.jpg",
+    desc: "CarbonFootprint is a Front-End application where users can calculate their carbon footprint.",
   },
 ];
 
